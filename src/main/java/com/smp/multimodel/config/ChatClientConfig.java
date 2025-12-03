ccd54b16eb07d73584f6f19ca41316dbb673e25f
sudhamohan <sudha.mohan.panda@rabobank.nl>
2025-12-03 19:44:33 +0100

    init

---
+++ src/main/java/com/smp/multimodel/config/ChatClientConfig.java
@@ -0,0 +1,23 @@
+package com.smp.multimodel.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        ChatClient.Builder chatClientBulder = ChatClient.builder(ollamaChatModel);
+        return chatClientBulder.build();
+    }
+
+}
